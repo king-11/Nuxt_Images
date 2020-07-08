3766c1177dba746e09cfad24c2b070648f8b49e9
king-11 <lakshay.singh1108@gmail.com>
2020-07-08 22:56:49 +0530

    navbar materialized

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,36 +1,61 @@
 <template>
-  <v-app-bar rounded app dark hide-on-scroll id="navbar">
-    <v-btn text to="/">Wallpaper Hub</v-btn>
-    <v-spacer />
-    <v-btn text to="/team">Team</v-btn>
-    <v-btn text to="/fests">Fests</v-btn>
-    <v-menu app bottom left transition="fab-transition">
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn dark icon v-bind="attrs" v-on="on">
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
-      </template>
-      <v-list>
-        <v-list-item v-if="isAuthenticated">
-          <v-list>
-            <v-list-item>{{ loggedInUser.username }}</v-list-item>
-            <v-list-item text to="/" @click="logout">Logout</v-list-item>
-          </v-list>
-        </v-list-item>
-        <v-list-item v-else>
-          <v-list>
-            <v-list-item text to="/login">Login</v-list-item>
-            <v-list-item text to="/register">Register</v-list-item>
-          </v-list>
+  <nav>
+    <v-app-bar app hide-on-scroll>
+      <v-app-bar-nav-icon class="ml-2">
+        <v-icon left large color="pink" @click="drawer = !drawer"
+          >mdi-menu</v-icon
+        >
+      </v-app-bar-nav-icon>
+      <v-toolbar-title text to="/" class="pl-0"
+        ><span class="headline font-weight-bold">Wallpaper</span>
+        <span class="subheading font-weight-light">Hub</span></v-toolbar-title
+      >
+      <v-spacer></v-spacer>
+      <v-btn
+        depressed
+        outline
+        class="pink white--text"
+        :to="isAuthenticated ? ' ' : '/login'"
+      >
+        <v-icon left>mdi-exit-to-app</v-icon>
+        <span v-if="isAuthenticated" @click="logout">SignOut</span>
+        <span v-else>SignIn</span>
+      </v-btn>
+    </v-app-bar>
+    <v-navigation-drawer app temporary absolute v-model="drawer">
+      <v-list nav>
+        <v-list-item
+          v-for="item in links"
+          :key="item.title"
+          link
+          :to="`/${item.route}`"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
       </v-list>
-    </v-menu>
-  </v-app-bar>
+    </v-navigation-drawer>
+  </nav>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 export default {
+  data() {
+    return {
+      links: [
+        { title: 'Dashboard', icon: 'mdi-view-dashboard', route: '' },
+        { title: 'Team', icon: 'mdi-account-group', route: '/team' },
+        { title: 'Fests', icon: 'mdi-party-popper', route: 'fests' },
+      ],
+      drawer: false,
+    }
+  },
   computed: { ...mapGetters(['isAuthenticated', 'loggedInUser']) },
   methods: {
     async logout() {
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,42 +1,41 @@
 <template>
-  <v-app dark>
+  <v-app>
     <Navbar />
     <v-main>
-    <Nuxt />
+      <Nuxt />
     </v-main>
   </v-app>
 </template>
 
-  <script>
-    import Navbar from '~/components/Navbar.vue'
-    export default {
-      components: {
-        Navbar,
-      },
-    }
-  </script>
+<script>
+import Navbar from '~/components/Navbar.vue'
+export default {
+  components: {
+    Navbar,
+  },
+}
+</script>
 
-  <style lang="scss">
-    * {
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-    }
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
-    .page-enter-active, .page-leave-active {
-      transition: all 0.8s ease-in;
-    }
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.8s ease-in;
+}
 
-    .page-enter {
-      transform: translateY(-100%) scaleX(0) translateZ(0);
-      transform-origin: left;
-      opacity: 0;
-    }
+.page-enter {
+  transform: translateY(-100%) scaleX(0) translateZ(0);
+  transform-origin: left;
+  opacity: 0;
+}
 
-    .page-leave-to {
-      transform: translateY(100%);
-      opacity: 0;
-    }
-
-  </style>
-</template>
+.page-leave-to {
+  transform: translateY(100%);
+  opacity: 0;
+}
+</style>
