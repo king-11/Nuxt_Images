051225cfec9a341fa6b4db9989441698f1b97b71
king-11 <lakshay.singh1108@gmail.com>
2020-08-21 18:29:55 +0530

    vuex updated for firebase auth
    social sign in actions created and instead of nuxt auth will use vuex only

--- store/index.js
+++ store/index.js
@@ -1,24 +1,54 @@
+import { auth, google, github, facebook } from '~/plugins/firebase'
+
+export const strict = false
+
+export const state = () => ({
+  user: null,
+})
+
 export const getters = {
   isAuthenticated(state) {
-    return state.auth.loggedIn
+    return !!state.user
   },
 
   loggedInUser(state) {
-    return state.auth.user
-  }
+    return state.user
+  },
+}
+
+export const mutations = {
+  setUser(state, user) {
+    state.user = user
+  },
 }
 
 export const actions = {
-  login: async ({
-    commit
-  }, user) => {
-    await this.auth.loginWith('local', {
-      data: {
-        ...user
-      }
-    })
-
-    this.router.push('/')
-  }
-
+  async logout({ commit }) {
+    await auth.signOut()
+    commit('setUser', null)
+  },
+  async loginGithub({ commit }) {
+    try {
+      const user = await auth.signInWithPopup(github)
+      commit('setUser', user.user)
+    } catch (err) {
+      console.log(err)
+    }
+  },
+  async loginGoogle({ commit }) {
+    try {
+      const user = await auth.signInWithPopup(google)
+      commit('setUser', user.user)
+    } catch (err) {
+      console.log(err)
+    }
+  },
+  async loginFacebook({ commit }) {
+    try {
+      const user = await auth.signInWithPopup(facebook)
+      commit('setUser', user.user)
+    } catch (err) {
+      console.log(err)
+    }
+  },
 }
