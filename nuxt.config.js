d1aced5daa02bc69cbb87f5da269adf0c3c5d6b5
king-11 <lakshay.singh1108@gmail.com>
2020-08-21 18:29:08 +0530

    plugins added to config
    firebase added as plugin in config for use at server load and config added

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,9 @@ export default {
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
    */
-  plugins: [],
+  plugins: [
+    '~/plugins/firebase.js'
+  ],
   /*
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
