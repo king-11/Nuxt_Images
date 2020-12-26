82a54143caa0bcc979d4324917cd2d55128d88f5
Lakshya Singh <lakshay.singh1108@gmail.com>
2020-12-26 20:20:39 +0530

    feat: disqus for comments and likes

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,7 +56,7 @@ export default {
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
    */
-  plugins: [],
+  plugins: ['~/plugins/disqus'],
   /*
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
